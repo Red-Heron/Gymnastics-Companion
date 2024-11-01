05421997dd9592ae0977e0eb6ba83b44fee5e76a
tbollen <thomas.bollen97@gmail.com>
2024-11-01 18:40:19 +0100

    added navbar components

---
+++ app/src/lib/components/navbar/navlink.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	// Import additional components
+	import Icon from '@iconify/svelte';
+
+	export let href = 'Home';
+	export let icon: string | undefined = undefined;
+</script>
+
+<a {href} class="nav-link">
+	{#if icon}<span class="icon"> <Icon {icon} /></span>{/if}
+	<span><slot /></span>
+</a>
+
+<style>
+	.nav-link {
+		position: relative;
+		/* Layout */
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		/* Font */
+		text-decoration: none;
+		color: inherit;
+		font-weight: 600;
+	}
+
+	.nav-link::after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		height: 2px;
+		width: 0%;
+		background-color: red;
+		transition: 0.3s;
+	}
+
+	.nav-link:hover::after {
+		left: 0;
+		width: 100%;
+	}
+</style>
